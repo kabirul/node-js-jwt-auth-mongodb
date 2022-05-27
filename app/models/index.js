e824653e1547e12ca486634a94c782241290e8d0
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-27 07:51:31 +0530

    Add files via upload

--- app/models/index.js
+++ app/models/index.js
@@ -5,8 +5,8 @@ const db = {};
 
 db.mongoose = mongoose;
 
-db.user = require("./user.model")(mongoose);;
-db.role = require("./role.model")(mongoose);;
+db.user = require("./user.model");
+db.role = require("./role.model");
 
 db.ROLES = ["user", "admin", "moderator"];
 
